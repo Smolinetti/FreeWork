5e21fe9fd7d53c33c9d135a0c71c7abbd50ef167
Smolinetti <2407373@mail.ru>
2022-09-06 13:40:28 +0300

    First work - Constructor.

--- src/main/java/sandbox/free/Main.java
+++ src/main/java/sandbox/free/Main.java
@@ -1,2 +1,20 @@
-package sandbox.free;public class main {
+package sandbox.free;
+
+public class Main {
+    public static void main(String[] args) {
+        Player player1 = new Player("Anton", 1500, 45, 10000);
+        Player player2 = new Player("Svetlana", 2000, 55, 20000);
+        Player player3 = new Player("Arsi");
+
+//        player1.nickname = "Anton";
+//        player2.nickname = "Svetlana";
+//        player3.nickname = "Arsi";
+
+        System.out.println();
+        System.out.println(player1.nickname + " и " + player2.nickname + " = " + player3.nickname);
+
+
+
+
+    }
 }
--- src/main/java/sandbox/free/Player.java
+++ src/main/java/sandbox/free/Player.java
@@ -1,2 +1,21 @@
-package sandbox.free;public class Player {
+package sandbox.free;
+
+public class Player {
+    String nickname;
+    int points;
+    int level;
+    int money;
+
+    public Player(String nickname, int points, int level, int money) {
+        this.nickname = nickname.toUpperCase();
+        this.points = points;
+        this.level = level;
+        this.money = money;
+    }
+
+    public Player(String nickname) {
+        this(nickname, 0, 1, 100);
+
+    }
+
 }
